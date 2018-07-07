98f3f7df3d50b33b9ec27d2d9893bdcbb23cf155
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-07-07 09:12:28 +0800

    Update version for new release 0.4

--- lib/CellBIS/SQL/Abstract.pm
+++ lib/CellBIS/SQL/Abstract.pm
@@ -6,7 +6,7 @@ use Mojo::Util qw(trim);
 use CellBIS::SQL::Abstract::Utils;
 
 # ABSTRACT: SQL Abstract
-our $VERSION = '0.3';
+our $VERSION = '0.4';
 
 # For Query Insert :
 # ------------------------------------------------------------------------
